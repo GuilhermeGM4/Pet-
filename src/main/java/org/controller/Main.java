ac324378874de09a6372cf218d44f747a5d4c123
GuilhermeGM4 <guilherme455@pm.me>
2024-06-10 18:35:20 -0300

    feat: created main inside controller package
    This was made with the purpose of testing

---
+++ src/main/java/org/controller/Main.java
@@ -0,0 +1,10 @@
+package org.controller;
+
+import org.controller.Cliente.ClienteController;
+
+public class Main {
+    public static void main(String[] args) {
+        ClienteController clienteTest = new ClienteController();
+        clienteTest.runTest();
+    }
+}

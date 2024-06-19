5a55f3c9b3ddee233c9832eff92b185cfe16d644
GuilhermeGM4 <guilherme455@pm.me>
2024-06-19 01:04:08 -0300

    feat: Implemented edit from VisualizarClienteController

--- src/main/java/org/controller/Cliente/VisualizarClienteController.java
+++ src/main/java/org/controller/Cliente/VisualizarClienteController.java
@@ -3,13 +3,12 @@ package org.controller.Cliente;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import org.UseCases.GerenciarCliente.EditarCliente;
 import org.model.Cliente;
 
 import java.util.Objects;
@@ -31,7 +30,16 @@ public class VisualizarClienteController extends Application {
     private Button btnVoltar;
 
     @FXML
+    private ChoiceBox<String> choiceSexo;
+
+    @FXML
     private TableView<?> tablePet;
+
+    @FXML
+    private TableColumn<?, ?> columnNome;
+
+    @FXML
+    private TableColumn<?, ?> columnRaca;
 
     @FXML
     private Button btnAdicionarPet;
@@ -45,11 +53,13 @@ public class VisualizarClienteController extends Application {
     @FXML
     private TextField txtfIdade;
 
-    Cliente client;
+    @FXML
+    private TextField txtfTelefone;
+
+    Cliente client = new Cliente("John Doe", "Masculino", 19, "12345678900", "12345678901");
 
     @Override
     public void start(Stage stage) throws Exception {
-        //TODO: corrigir erro que "visualizar_cliente.fxml" nao esta sendo achado
         final Pane graph = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("visualizar_cliente.fxml")));
         final Scene scene = new Scene(graph, 800, 600);
         stage.setTitle("Gerenciar Clientes");
@@ -59,46 +69,81 @@ public class VisualizarClienteController extends Application {
 
     @FXML
     public void initialize() {
-        //TODO: setar todos os dados dos campos
-        txtfNome.setText(client.getNome());
-        txtfCpf.setText(client.getCpf());
-        txtfIdade.setText(Integer.toString(client.getIdade()));
+        choiceSexo.getItems().addAll("Masculino", "Feminino", "Outro");
+        setDefaultValues();
+    }
+
+    @FXML
+    void edit(ActionEvent event) {
+        EditarCliente editor = new EditarCliente();
+        String result = editor.editar(
+                txtfNome.getText(),
+                txtfCpf.getText(),
+                choiceSexo.getValue(),
+                Integer.parseInt(txtfIdade.getText()),
+                txtfTelefone.getText()
+        );
+        //TODO: adicionar um text na janela para mostrar mensagens do sistema e implementar
+        if(!result.equals("Cliente alterado com sucesso")){
+            System.out.println(result);
+        }
+        btnIniciaEdicao.setText("Iniciar Edição");
+        changeEditableStatus(false);
     }
 
     @FXML
     void addPet(ActionEvent event) {
-
+        //TODO: implementar
     }
 
     @FXML
     void backwards(ActionEvent event) {
-
+        //TODO: implementar
     }
 
     @FXML
     void handleEditionButton(ActionEvent event) {
         if(btnIniciaEdicao.getText().equals("Iniciar Edição")) {
             btnIniciaEdicao.setText("Cancelar Edição");
-            btnEditar.setDisable(false);
-            btnVisualizar.setDisable(false);
+            changeEditableStatus(true);
         } else {
-            //TODO: voltar os campos para o estado inicial utilizando o obj client
             btnIniciaEdicao.setText("Iniciar Edição");
-            btnEditar.setDisable(true);
-            btnVisualizar.setDisable(true);
+            changeEditableStatus(false);
+            setDefaultValues();
         }
+    }
+
+    private void changeEditableStatus(boolean status){
+        btnEditar.setDisable(!status);
+        txtfNome.setEditable(status);
+        txtfIdade.setEditable(status);
+        txtfTelefone.setEditable(status);
+        choiceSexo.setDisable(!status);
+    }
+
+    private void setDefaultValues(){
+        txtfNome.setText(client.getNome());
+        txtfCpf.setText(client.getCpf());
+        txtfIdade.setText(Integer.toString(client.getIdade()));
+        txtfTelefone.setText(client.getTelefone());
+
+        choiceSexo.setValue(client.getSexo());
     }
 
     @FXML
     void removePet(ActionEvent event) {
-
+        //TODO: implementar
     }
 
     @FXML
     void viewPet(ActionEvent event) {
-
+        //TODO: implementar
     }
-//TODO: mockar um cliente para fazer testes
+
+    private void populateTable(){
+        //TODO: implementar
+    }
+
     public void setClient(Cliente c){
         client = c;
     }

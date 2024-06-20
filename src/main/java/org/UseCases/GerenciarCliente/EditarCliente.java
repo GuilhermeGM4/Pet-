5f1f8b341bc3c80cf4036a1c352beaaf14ec7b48
GuilhermeGM4 <guilherme455@pm.me>
2024-06-20 18:14:31 -0300

    feat: Implemented DAO into EditarCliente

--- src/main/java/org/UseCases/GerenciarCliente/EditarCliente.java
+++ src/main/java/org/UseCases/GerenciarCliente/EditarCliente.java
@@ -1,27 +1,35 @@
 package org.UseCases.GerenciarCliente;
 
+import org.DAO.Cliente.ClienteDAO;
 import org.model.Cliente;
 
 public class EditarCliente {
     //TODO: retirar sets diretos em client e levar eles para o DAO
+    ClienteDAO dao = new ClienteDAO();
     Cliente client = new Cliente("John Doe", "Masculino", 19, "12345678900", "12345678901");
-    public String editar(String newName, String cpf, String newPhone, int newAge, String newGender){
-        if(newName.isEmpty() || newPhone.isEmpty() || newGender.isEmpty()) return "Nenhum campo deve estar vazio.";
-        if(newAge < 18) return "O cliente deve ter mais de 18 anos.";
+
+    public String editar(String newName, String cpf, String newAge, String newGender, String newPhone){
+        if(newName.isEmpty() || newPhone.isEmpty() || newGender.isEmpty() || newAge.isEmpty()) return "Nenhum campo deve estar vazio.";
+        if(Integer.parseInt(newAge) < 18) return "O cliente deve ter mais de 18 anos.";
         if(!(newGender.equals("Masculino") || newGender.equals("Feminino") || newGender.equals("Outro"))){
             return "Sexo inválido";
         }
-        if(newPhone.length()!= 11) return "Telefone deve ter 11 digitos.";
+        if(newPhone.length() != 11) return "Telefone deve ter 11 digitos.";
 
         String name = !newName.equals(client.getNome())? newName: client.getNome();
         String gender = !newGender.equals(client.getSexo())? newGender: client.getSexo();
-        int age = newAge != client.getIdade()? newAge: client.getIdade();
+        int age = Integer.parseInt(newAge) != client.getIdade()? Integer.parseInt(newAge): client.getIdade();
         String phone = !newPhone.equals(client.getTelefone())? newPhone: client.getTelefone();
-        Cliente editedClient = new Cliente(name, gender, age, cpf, phone); //utilizar no DAO
-        client.setNome(name);
-        client.setSexo(gender);
-        client.setIdade(age);
-        client.setTelefone(phone);
-        return "Modificado com sucesso";
+        Cliente editedClient = new Cliente(name, gender, age, client.getCpf(), phone);
+        return dao.alterar(editedClient);
+//        client.setNome(name);
+//        client.setSexo(gender);
+//        client.setIdade(age);
+//        client.setTelefone(phone);
+//        return "Modificado com sucesso";
+    }
+
+    public void setClient(Cliente client){
+        this.client = client;
     }
 }

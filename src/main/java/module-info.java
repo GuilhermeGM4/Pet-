abd2f3cda8121451fb40613350ccbe98b50f5baa
GuilhermeGM4 <guilherme455@pm.me>
2024-06-19 21:02:19 -0300

    doc: updated module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,11 +4,15 @@ module PetPlus {
     requires javafx.fxml;
     requires javafx.graphics;
 
+    opens org.controller;
     opens org.controller.Cliente;
 
     opens org.model;
+    opens org.Utils;
 
     exports org.controller.Cliente;
 
     exports org.model;
+    exports org.controller;
+    exports org.Utils;
 }

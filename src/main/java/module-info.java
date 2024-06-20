0d0c86ea9c161ed60dbf695c9e50c0ef3d7aab6f
GuilhermeGM4 <guilherme455@pm.me>
2024-06-20 18:17:38 -0300

    doc: Added new packages to module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,13 +6,15 @@ module PetPlus {
 
     opens org.controller;
     opens org.controller.Cliente;
+    opens org.controller.Pet;
 
     opens org.model;
     opens org.Utils;
 
+    exports org.controller;
     exports org.controller.Cliente;
+    exports org.controller.Pet;
 
     exports org.model;
-    exports org.controller;
     exports org.Utils;
 }

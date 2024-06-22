a4ed0a78bc8765cc1eac8837b9e310cc3ad5380e
GuilhermeGM4 <guilherme455@pm.me>
2024-06-22 14:51:19 -0300

    fix: Fixed multiple wrong return types in LoaderDAOInterface
    The methods were returning the raw objects when they should return an array of those objects.

--- src/main/java/org/DAO/LoaderDAOInterface.java
+++ src/main/java/org/DAO/LoaderDAOInterface.java
@@ -8,16 +8,13 @@ import org.model.Servico;
 import java.util.ArrayList;
 
 public interface LoaderDAOInterface {
-    public Cliente loadClientData();
-    public Funcionario loadEmployeeData();
-    public Servico loadServiceData();
-    public Estoque loadInventoryData();
+    public ArrayList<Cliente> loadClientData();
+    public ArrayList<Funcionario> loadEmployeeData();
+    public ArrayList<Servico> loadServiceData();
+    public ArrayList<Estoque> loadInventoryData();
 
     public void writeClientsData(ArrayList<Cliente> clients);
     public void writeEmployeeData(ArrayList<Funcionario> employees);
     public void writeServiceData(ArrayList<Servico> services);
     public void writeInventoryData(ArrayList<Estoque> inventory);
-
-    public void writeData();
-
 }

9ac03b54d8a9fdaa72d31ce1b2f7f13df69cd7ad
GuilhermeGM4 <guilherme455@pm.me>
2024-06-23 15:26:05 -0300

    feat: Added some methods to Estoque

--- src/main/java/org/model/Estoque.java
+++ src/main/java/org/model/Estoque.java
@@ -1,16 +1,35 @@
 package org.model;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Estoque {
-    private Map<String, Float> produtos;
-    private Map<String, Integer> quantidadeNoEstoque;
+    private final Map<String, Float> produtos = new HashMap<String, Float>();
+    private final Map<String, Integer> quantidadeNoEstoque = new HashMap<String, Integer>();
 
     public Estoque() {
     }
 
-    public void adicionarProduto(){}
+    public void adicionarProduto(String nome, float preco, int quantidade){
+        produtos.put(nome, preco);
+        quantidadeNoEstoque.put(nome, quantidade);
+    }
+
+    public Map<String, Float> getProdutos(){
+        return produtos;
+    }
+
+    public Map<String, Integer> getQuantidadeNoEstoque(){
+        return quantidadeNoEstoque;
+    }
+
+    public Integer getQtdProduto(String nome){
+        return quantidadeNoEstoque.get(nome);
+    }
+
+    public Float getPrecoProduto(String nome){
+        return produtos.get(nome);
+    }
 
     public void removerItem(){}
 
